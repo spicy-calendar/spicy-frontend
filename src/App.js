3319b3a484ddd69cf3ce5b462569e793a5c57be6
Weisheng Wu <weishenggwuu@gmail.com>
2019-03-02 14:34:20 -0800

    added page resizable  divider

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from "./containers/Login";
+import Calendar from './containers/Calendar';
 
 class App extends Component {
   render() {
@@ -10,7 +11,7 @@ class App extends Component {
         <header className="App-header">
           <h3>SMART CALENDAR</h3>
         </header>
-        <Login/>
+        <Calendar/>
       </div>
     );
   }
