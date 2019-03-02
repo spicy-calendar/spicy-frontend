a4e1f23f8e7c4ae6cdfabdfd03720f80b04adeda
Ken Le <kle9597@gmail.com>
2019-03-02 13:01:59 -0800

    Added code for login page

---
+++ src/containers/Login.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import { EditorFormatAlignCenter } from "material-ui/svg-icons";
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username : "",
+      password : ""
+    }
+  }
+
+  render() {
+    const { validated } = this.state;
+    return (
+      <div>
+        <MuiThemeProvider>
+          <div>
+          <AppBar
+             title="Smart Calendar"
+           />
+           <TextField
+             hintText="Enter your Username"
+             floatingLabelText="Username"
+             style={style}
+             onChange = {(event,newValue) => this.setState({username:newValue})}
+             />
+           <br/>
+             <TextField
+               type="password"
+               hintText="Enter your Password"
+               floatingLabelText="Password"
+               style={style}
+               onChange = {(event,newValue) => this.setState({password:newValue})}
+               />
+             <br/>
+             <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+         </div>
+         </MuiThemeProvider>
+      </div>
+    );
+  }
+}
+
+const style = {
+  margin : "20px 0px 0px 90px",
+};
